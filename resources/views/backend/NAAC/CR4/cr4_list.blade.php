d98ef13ce5d6a2b80ae372e5de1ad9b7f5613022
Nisarahmed <nwebdevelopment2023@gmail.com>
2025-04-19 21:18:06 +0530

    all CR 1 to 7 has been created

---
+++ resources/views/backend/NAAC/CR4/cr4_list.blade.php
@@ -0,0 +1,49 @@
+@extends('backend.layouts.app')
+@section('style')
+@endsection
+
+@section('content')
+
+
+<div class="pagetitle">
+ <!-- Table with stripped rows -->
+ @include('auth._message')
+ <div class="card">
+    <div class="card-body">
+
+      <h5 class="card-title">Criterion 1 Documents
+        <a href="{{ route('cr1_insert') }}" class="btn btn-primary sm-btn" style="float: right">Add</a>
+      </h5>
+     <table class="table table-striped">
+        <thead>
+          <tr>
+            <th scope="col">#</th>
+            <th scope="col">File Name</th>
+            <th scope="col">Metric No.</th>
+            <th scope="col">Action</th>
+          </tr>
+        </thead>
+        <tbody>
+            @foreach ($getRecords as $pdf)
+                <tr>
+                    <th scope="row">{{ $pdf->id }}</th>
+                    <td>{{ $pdf->title }}</td>
+                    <td>{{ $pdf->metrics }}</td>
+                    <td>
+                        <a class="btn btn-primary btn-sm" href="{{ asset('storage/pdfs/' . $pdf->title) }}" target="_blank">view</a>
+                        <a class="btn btn-secondary btn-sm" href="{{ url('cr1_edit/'.$pdf->id) }}"><i class="bi bi-pencil"></i></a>
+                        <a onclick="return confirm('Do you want to delete?')" class="btn btn-danger btn-sm" href="{{ url('cr1_delete/'.$pdf->id) }}"><i class="bi bi-trash3-fill"></i></a>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
+        {!! $getRecords->appends(Illuminate\Support\Facades\Request::except('page'))->links() !!}
+      </table>
+
+     </div>
+  </div>
+  <!-- End Table with stripped rows -->
+  @endsection
+  @section('script')
+  @endsection
+
